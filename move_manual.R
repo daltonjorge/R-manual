4dcd22f69fc187f466941297506f3caabac6f253
Ken Kellner <ken@kenkellner.com>
2017-05-30 13:14:47 -0400

    Finish first draft of manual

---
+++ move_manual.R
@@ -0,0 +1,9 @@
+if(!file.exists('filepath.R')){
+  require(tcltk)
+  file.dir <- tk_choose.dir()
+  out.string <- paste("filepath <- \"",file.dir,"\"",sep="")
+  cat(out.string,file="filepath.R",sep="\n")
+}
+source('filepath.R')
+file.copy('R_Instruction_Manual.html',filepath)
+file.remove('R_Instruction_Manual.html')
